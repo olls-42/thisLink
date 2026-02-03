f81a91a7a151770528e89ffc55699cdad169d78f
olls-42 <oleksandr.slobodianiuk.84@gmail.com>
2026-02-03 19:47:26 +0200

    run devcontainer

---
+++ api/src/Controller/LuckyController.php
@@ -0,0 +1,19 @@
+<?php
+
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LuckyController  extends AbstractController
+{
+    #[Route('/lucky/number/{max}', name: 'app_lucky_number')]
+    public function number(int $max): JsonResponse
+    {
+        $number = random_int(0, $max);
+
+        return $this->json(['number' => $number]);
+    }
+}
